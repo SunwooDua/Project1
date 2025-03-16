6cd81daa48da7b702745bbd1571c65fbdf701585
Sunwoo Yeom <o55306605@gmail.com>
2025-03-15 21:55:42 -0400

    set up for database_helper

--- lib/database_helper.dart
+++ lib/database_helper.dart
@@ -2,4 +2,46 @@ import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path_provider/path_provider.dart';
 
-class DatabaseHelper {}
+class DatabaseHelper {
+  // refer to database helper from icollege
+  static Database? _db;
+  //singleton instance
+  static final DatabaseHelper instance = DatabaseHelper._constructor();
+
+  final String table = 'recipe_table';
+  final String columnId = '_id';
+  final String columnName = 'name';
+  final String columnIngredients = 'ingredients';
+  final String columnInstructions = 'instructions';
+
+  // create private constructor for Database
+  DatabaseHelper._constructor();
+
+  Future<Database> get database async {
+    if (_db != null) return _db!; // if not nul return db
+    _db = await getDatabase(); //else get database (open)
+    return _db!;
+  }
+
+  //set up database
+  Future<Database> getDatabase() async {
+    //where will be database?
+    final databaseCreatePath = await getDatabasesPath();
+    final databasePath = join(databaseCreatePath, "recipe_db.db");
+    final database = await openDatabase(
+      // actual function and logic for database
+      databasePath,
+      onCreate: (db, version) {
+        db.execute('''
+        CREATE TABLE $table (
+          $columnId INTEGER PRIMARY KEY, 
+          $columnName TEXT NOT NULL,
+          $columnIngredients TEXT,
+          $columnInstructions TEXT,
+        )
+        ''');
+      },
+    );
+    return database;
+  }
+}
