86fe9874324ba6143407b990f8e7c3375761f2bf
Sunwoo Yeom <o55306605@gmail.com>
2025-03-16 02:10:36 -0400

    checking with multiple recipe

--- lib/recipedata.dart
+++ lib/recipedata.dart
@@ -14,4 +14,13 @@ List<Map<String, dynamic>> recipes = [
     'type':
         'vegan & gluten-free', // need to be string in order to be used in sql database
   },
+  {
+    'name': 'Gochujang Egg Rice',
+    'ingredients':
+        'Gochujang (2Tbsp or more), Eggs (usually 2), Rice (1 bowl), Sesame Oil (1Tbsp or more)',
+    'instruction':
+        'prepare rice (steam), prepare fried eggs, prepare spoon of gochujang sauce and ssame oil, mix them all and enjoy!',
+    'type':
+        'vegetarian', // need to be string in order to be used in sql database
+  },
 ];
